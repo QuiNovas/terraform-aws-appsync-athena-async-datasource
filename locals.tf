211a090841ea38e557aa18c57cbdeba4dfaeab50
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2020-02-27 16:48:47 +0530

    flip if condition

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 local {
-  policy_arns = var.athena_s3_staging_arn != "" ? ["arn:aws:iam::aws:policy/AmazonAthenaFullAccess", var.athena_datasource_policy_arn,
+  policy_arns = var.athena_s3_staging_arn == "" ? ["arn:aws:iam::aws:policy/AmazonAthenaFullAccess", var.athena_datasource_policy_arn,
   ] : ["arn:aws:iam::aws:policy/AmazonAthenaFullAccess", var.athena_datasource_policy_arn, aws_iam_policy.non_default_staging_dir_access.arn]
 }
